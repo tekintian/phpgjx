b8eabae43f949a6ffca1c2e2864fc29c3a3dae1e
tekintian <tekintian@gmail.com>
2018-02-10 19:30:05 +0800

    增加Mac docker容器配置示例

--- README.md
+++ README.md
@@ -20,5 +20,66 @@ php工具箱 永久开源,免费升级
 
 本工具 只兼容 最新版  IE 火狐 谷歌
 
+## docker容器中使用本工具方法
+配置环境为tengine和mysql都是在Docker 容器中运行的
+
+###———————————————tengine——————————————
+
+	docker run  -d -it \
+		--name tengine  \
+		--restart=always \
+		-p 80:80 \
+		-p 443:443 \
+		-v /Users/Tekin/web:/home/wwwroot  \
+		-v /Users/Tekin/logs:/home/wwwlogs \
+		tekintian/tengine-php:7.0.22
+
+###———————————————mariadb——————————————
+	https://hub.docker.com/_/mariadb/
+	/etc/mysql  #mariadb 配置文件
+	/var/lib/mysql  # 数据库文件存放目录
+	/var/log/mysql/mysql_bz.log #日志文件夹
+	/Users/Tekin/logs/mariadb-10.1 
+	把日志文件放到tengine的日志目录中， 方便使用phpgjx 调用地址为  /home/wwwlogs/mariadb-10.1/mysql_bz.log 因为tengine设置的日志映射目录为 /home/wwwlogs/
+
+	—————————— mariadb-10.1
+	docker run  -d -it \
+		--name mariadb  \
+		--restart=always \
+		-p 3306:3306 \
+		-v /Users/Tekin/docker/opt/mariadb10.1/data:/var/lib/mysql \
+		-v /Users/Tekin/docker/opt/mariadb10.1/etc:/etc/mysql \
+		-v /Users/Tekin/Desktop/web/_logs/mariadb-10.1:/var/log/mysql \
+		 -e MYSQL_ROOT_PASSWORD=888888 \
+		mariadb:10.1
+
+
+### mariadb my.cnf 配置
+在[mysqld] 节点下增加general_log日志配置
+
+	[mysqld]
+	#
+	# * Character sets
+	# 
+	# Default is Latin1, if you need UTF-8 set all this (also in client section)
+	#
+	character-set-server  = utf8 
+	collation-server      = utf8_general_ci 
+	character_set_server   = utf8 
+	collation_server       = utf8_general_ci 
+
+	#general log for phpgjx debug
+	general_log_file        = /var/log/mysql/mysql_bz.log
+	general_log             = 1
+
+	log-raw=true #记录错误日志
+
+## coonfig.php 配置文件配置
+
+	'mysql_log'=>'/home/wwwlogs/mariadb-10.1/mysql_bz.log', // mysql 标准日志文件路径			
+    'mysql_host' =>'localhost', //mysql连接地址
+	'mysql_user' =>'root', //mysql账号
+	'mysql_password' =>'888888', //mysql密码
+	'mysql_port' =>'3306', //mysql端口	
 
 
--- config.php
+++ config.php
@@ -31,12 +31,12 @@ general_log_file=D:/wamp/www/mysql_bz.log // 这里设置你 log日志路径  �
 如果需要记录所有的语句，包括那些错误的，请加 log-raw选项  log-raw=true
 */
 return array(
-	'web_url'  =>'http://127.0.0.1/phpgjx/index.php', // php工具箱访问 url 根路径  能访问到你工具箱的 地址
-	'mysql_log'=>'D:/mysql_bz2.log', // mysql 标准日志文件路径			
-    'mysql_host' =>'localhost', //mysql连接地址
-	'mysql_user' =>'root', //mysql账号
-	'mysql_password' =>'', //mysql密码
-	'mysql_port' =>'3306', //mysql端口	
+	'web_url'  =>'http://tools.dd/phpgjx/index.php', // php工具箱访问 url 根路径  能访问到你工具箱的 地址
+	'mysql_log'=>'/home/wwwlogs/mariadb-10.1/mysql_bz.log', // mysql 标准日志文件路径			
+    'mysql_host' =>'172.17.0.1', //mysql连接地址
+	'mysql_user' =>'tools', //mysql账号
+	'mysql_password' =>'tools888', //mysql密码
+	'mysql_port' =>'3307', //mysql端口	
 	'menu'=>array( // 菜单配置项
 	     '在线工具'=>'http://tool.oschina.net/',   // 如果自己配置的地址打不卡 请加上 http://   如 www.baidu.com  打不开 前面加上  http://www.baidu.com
 	     '工具百宝箱'=>'http://tool.lu/',  		 
@@ -47,31 +47,14 @@ return array(
 		 'TP手册'=>'http://document.thinkphp.cn/manual_3_2.html',  		 		 		 
 		 '正则测试'=>'http://tool.oschina.net/regex',		 
 		 '代码比对'=>'http://tool.oschina.net/diff',
-		 'JSON格式化'=>'http://tool.oschina.net/codeformat/json',
+		 'JSON格式化'=>'https://www.json.cn/',
 		 'phpinfo'=>'phpinfo.php', 
 		 'php探针'=>'tz.php',
 		 '运行代码'=>'run.php',
+		 'Adminer'=>'adminer.php',
 		 'phpmyadmin'=>'phpmyadmin.php',
 		 'php手册'=>'http://www.w3school.com.cn/php/index.asp',  // http://www.tp-shop.cn/phpgjx/ip_city.php
 		 '文件复制'=>'file_copy.php',
-		 'TPshop官网'=>'http://www.tp-shop.cn',
+		 '云南网'=>'http://yunnan.ws',
 	),
 );
-
-
-/* 如果学了有不明白的地方 加以下群, 有专门老师免费辅导 */
-
-/**   
-php北京群:224612563
-php上海群:203320537
-php深圳2群:209669661
-php深圳1群:182127670
-php杭州群:209983691
-php成都群:488097296
-php广州群:364702379
-php武汉群:312123912
-php南京 496341851
-php重庆 243249226
-ecshop群:216961593 
-TPshop官方群:571797355
-*/
